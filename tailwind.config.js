14c321b6dd3e1b6064cac2f9ba599c5cbca443fb
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2023-11-29 21:39:55 +0530

    frontend-mentor-challenge-complete

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'tomato': 'hsl(4, 100%, 67%)',
+        'dark-slate-grey': 'hsl(234, 29%, 20%)',
+        'charcoal-grey': 'hsl(231, 7%, 60%)',
+      },
+      fontFamily: {
+        roboto: ['Roboto'],
+      },
+    },
+    fontWeight: {
+      normal: 400,
+      bold: 700,
+    },
+  },
+  plugins: [],
+};
